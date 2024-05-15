b14f1750c3fb0fbe42e40bbcf2fae4d397f8de0d
potatocat7 <potatocat7@gmail.com>
2024-05-15 14:04:54 +0900

    エンティティ作成

---
+++ Assets/Script/EnemyEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//エディターで呼び出せるように
+[CreateAssetMenu(fileName = "EnemyEntity", menuName = "Create EnemyEntity")]
+public class EnemyEntity : ScriptableObject
+{
+    public int MHP;
+    public int ATK;
+    public int LV;
+    public int EXP;
+    public int MEXP;
+}
+
